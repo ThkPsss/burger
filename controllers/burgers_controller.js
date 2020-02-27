1684177b596a5983cf2a396ccc0d0266e1800207
ThkPsss <christanspence66@gmail.com>
2020-02-27 17:15:10 -0500

    Controller fixes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,38 +16,26 @@ router.get("/", function(req, res) {
     });
   });
 
-  router.post("/api/burgers", function(req, res) {
-    burger.insertOne([
-      "name", "eatten"
-    ], [
-      req.body.name, req.body.eatten
-    ], function(result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-    });
+  router.post("/", function(req, res) {
+    console.log(req.body.name)
+    if(req.body.name !== ""){
+      burger.insertOne(req.body.name.trim(), function(){
+        res.redirect("/")
+      })
+    }
   });
 
-  router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    console.log("condition", condition);
-  
-    burger.updateOne({
-      eatten: req.body.eatten
-    }, condition, function(result) {
-      if (result.changedRows == 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      } else {
-        res.status(200).end();
-      }
-    });
+  router.put("/:id", function(req, res) {
+    console.log(req.params.id)
+    burger.updateOne(req.params.id, function () {
+      res.redirect("/")
+    })
   });
 
   router.delete("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
   
-    cat.delete(condition, function(result) {
+    burger.delete(condition, function(result) {
       if (result.affectedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
